b949142a3292e589ca12a3a1a3b807e601ddce38
YK <molodoy@li.ru>
2023-06-13 00:14:32 +0300

    Unit-tests Ttl

---
+++ tests/Test.Ttl/TtlDictionaryTests.cs
@@ -0,0 +1,48 @@
+﻿using Simple.Ttl;
+
+using Xunit;
+
+namespace Test.Ttl
+{
+    public class TtlDictionaryTests
+    {
+        private const string Key = "test key";
+        private static readonly TimeSpan ttl = TimeSpan.FromSeconds(1);
+        private readonly TtlDictionary<string> svc = new(ttl);
+
+        [Fact]
+        public void GetOrCreate()
+        {
+            //  arrange
+            var expected = 0;
+            var factory = (string s) => ++expected;
+
+            //  test
+            var actual = svc.GetOrCreate(Key, factory);
+            Thread.Sleep(ttl);
+            var actual2 = svc.TryGetValue(Key, out int _);
+
+            //  assert
+            Assert.Equal(expected, actual);
+            Assert.False(actual2);
+        }
+
+        [Fact]
+        public void Set()
+        {
+            //  arrange
+            var expected = 16;
+
+            //  test
+            var actual = svc.Set(Key, expected);
+            var actual1 = svc.TryGetValue(Key, out int _);
+            Thread.Sleep(ttl);
+            var actual2 = svc.TryGetValue(Key, out int _);
+
+            //  assert
+            Assert.Equal(expected, actual);
+            Assert.True(actual1);
+            Assert.False(actual2);
+        }
+    }
+}
---
+++ tests/Test.Ttl/TtlValueTests.cs
@@ -0,0 +1,158 @@
+using Simple.Ttl;
+
+using Xunit;
+
+namespace Test.Ttl
+{
+    public class TtlValueTests
+    {
+        private static readonly TimeSpan ttl = TimeSpan.FromSeconds(0.2);
+        private readonly TtlValue<int> svc = new TtlValue<int>(ttl);
+
+        [Fact]
+        public void GetOrCreate()
+        {
+            //  arange
+            var runCount = 0;
+            var factory = () => ++runCount;
+
+            //  test
+            var actual0 = svc.GetOrCreate(factory);
+            var actual1 = svc.GetOrCreate(factory);
+            Thread.Sleep(ttl);
+            var actual2 = svc.GetOrCreate(factory);
+
+            //  assert
+            Assert.Equal(1, actual0);
+            Assert.Equal(1, actual1);
+            Assert.Equal(2, actual2);
+            Assert.Equal(2, runCount);
+        }
+
+        [Fact]
+        public async void GetOrCreateAsync()
+        {
+            //  arange
+            var runCount = 0;
+            var factory = () => Task.FromResult(++runCount);
+
+            //  test
+            var actual0 = await svc.GetOrCreateAsync(factory);
+            var actual1 = await svc.GetOrCreateAsync(factory);
+            await Task.Delay(ttl);
+            var actual2 = await svc.GetOrCreateAsync(factory);
+
+            //  assert
+            Assert.Equal(1, actual0);
+            Assert.Equal(1, actual1);
+            Assert.Equal(2, actual2);
+            Assert.Equal(2, runCount);
+        }
+
+        [Theory]
+        [InlineData(10, 2, 5)]
+        //[InlineData(100, 10, 10)]     //  +1 - corretion with time of executing test (ttl = 0:00:01.0)
+        //[InlineData(100, 10, 13)]     //  +1 - corretion with time of executing test (ttl = 0:00:00.5)
+        [InlineData(100, 10, 15)]     //  +1 - corretion with time of executing test (ttl = 0:00:00.2)
+        public void MultithteadCount(int iterations, int parts, int expected)
+        {
+            //  arrange
+            var runCount = 0;
+            var factory = () => ++runCount;
+            var actuals = new List<int>(iterations);
+            var partTtl = ttl / parts;
+
+            //  test
+            while (iterations-- > 0)
+            {
+                actuals.Add(svc.GetOrCreate(factory));
+                Thread.Sleep(partTtl);
+            }
+
+            //  assert
+            var d = actuals.GroupBy(x => x).Select(i => $"{i.Key} -> {i.Count()}");
+            //Trace.TraceError(string.Join('\n', d));
+            Assert.Equal(expected, d.Count());
+            Assert.Equal(expected, runCount);
+        }
+
+        [Theory]
+        [InlineData(10, 2, 5)]
+        [InlineData(100, 10, 15)]     //  +1 - corretion with time of executing test
+        public async Task MultithteadCountAsync(int iterations, int parts, int expected)
+        {
+            //  arrange
+            var runCount = 0;
+            var factory = () => Task.FromResult(++runCount);
+            var actuals = new List<int>(iterations);
+            var partTtl = ttl / parts;
+
+            //  test
+            while (iterations-- > 0)
+            {
+                actuals.Add(await svc.GetOrCreateAsync(factory));
+                await Task.Delay(partTtl);
+            }
+
+            //  assert
+            var d = actuals.GroupBy(x => x).Select(i => $"{i.Key} -> {i.Count()}");
+            //Trace.TraceError(string.Join('\n', d));
+            Assert.Equal(expected, d.Count());
+            Assert.Equal(expected, runCount);
+        }
+
+        [Fact]  //  TODO: Test is not stable
+        public async Task MultithteadDuration()
+        {
+            //  arrange
+            var runCount = 0;
+            var factory = () => Task.FromResult(++runCount);
+            var expired = DateTime.UtcNow + ttl;
+
+            //  test
+            while (expired > DateTime.UtcNow)
+            {
+                await svc.GetOrCreateAsync(factory);
+            }
+
+            //  assert
+            Assert.Equal(1, runCount);
+        }
+
+        [Fact]
+        public void Set()
+        {
+            //  arange
+            const int value = 14;
+
+            //  test
+            svc.Set(value);
+            var actual1 = svc.TryGetValue(out var v1);
+            Thread.Sleep(ttl);
+            var actual2 = svc.IsExpired;
+
+            //  assert
+            Assert.Equal(value, v1);
+            Assert.True(actual1);
+            Assert.True(actual2);
+        }
+
+        [Fact]
+        public void TryGetValue()
+        {
+            //  arange
+            const int value = 14;
+            svc.Set(value);
+
+            //  test
+            var actual1 = svc.TryGetValue(out var v1);
+            Thread.Sleep(ttl);
+            var actual2 = svc.TryGetValue(out var v2);
+
+            //  assert
+            Assert.Equal(value, v1);
+            Assert.True(actual1);
+            Assert.False(actual2);
+        }
+    }
+}
