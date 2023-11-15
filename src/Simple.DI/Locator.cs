c76e4b1b3866bfd7b37413d762ef8b24424f929c
YK <molodoy@li.ru>
2023-11-15 16:33:04 +0200

    update Resolver

--- src/Simple.DI/Locator.cs
+++ src/Simple.DI/Locator.cs
@@ -2,9 +2,9 @@
 
 namespace Simple.DI
 {
-    public class Locator : Resolver<Type>, IProviderSetup, IServiceProvider
+    public class Locator
     {
-        private static readonly Locator _instance = new();
+        private static readonly Resolver _instance = new();
 
         /// <summary> Gets the <see cref="IServiceProvider" /> that provides access to the application's service container </summary>
         public static IServiceProvider Current => _instance;
@@ -13,25 +13,11 @@ namespace Simple.DI
         public static IProviderSetup Setup() => _instance;
 
 
-        protected Locator() : base(null)
-        {
-        }
-
-        #region IProviderSetup
-
-        public IProviderSetup Register(Type key, Func<IServiceProvider, object?> factory)
-            => (IProviderSetup)base.Register(key, () => factory(this));
-
-        public IServiceProvider BuildServiceProvider()
-            => this;
-
-        #endregion
-
         #region IServiceProvider
 
         //  Replacing the key from 'ILogget<T>' on 'ILogget<>'
-        public override object? GetService(Type key)
-            => base.GetService(key.IsGenericType ? key.GetGenericTypeDefinition() : key);
+        public object? GetServiceGeneric(Type key)
+            => _instance.GetService(key.IsGenericType ? key.GetGenericTypeDefinition() : key);
 
         #endregion
     }
