4742108675ce822c2e6dcaa62f8c19958c9275f7
Prog75 <mail@yk.cc.ua>
2024-07-21 07:54:31 +0300

    gitignore +

--- clean2.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-for /f %%a in ('dir /s /b /a:D "bin" "obj" "ProjectEvaluation" "dataSources" "target"') do rd /s /q %%a & echo %%a
-
-rem  .vs/ProjectEvaluation, .idea/dataSources, rust-tests/target
-
-pause
