6d6d28da268f0ca372134a437630da7cb8b66b91
Prog75 <mail@yk.cc.ua>
2024-09-20 20:46:35 +0300

    WebJwt: validator

--- src/Simple.Web.Jwt/IBase64UrlEncoder.cs
+++ src/Simple.Web.Jwt/IBase64UrlEncoder.cs
@@ -4,10 +4,10 @@ namespace Simple.Web.Jwt;
 public interface IBase64UrlEncoder
 {
     /// <summary> Encodes the byte array to a base64 string. </summary>
-    string Encode(byte[] input);
+    bool TryEncode(byte[] input, out string output, out string? error);
 
     /// <summary> Decodes the base64 string to a byte array. </summary>
-    byte[] Decode(string input);
+    bool TryDecode(string input, out byte[] output, out string? error);
 }
 
 //  TODO: use Span
@@ -15,43 +15,111 @@ public class Base64UrlEncoder : IBase64UrlEncoder
 {
     /// <inheritdoc />
     /// <exception cref="ArgumentNullException" />
-    /// <exception cref="ArgumentOutOfRangeException" />
-    public string Encode(byte[] input)
+    public bool TryEncode(byte[] input, out string output, out string? error)
     {
-        Throw.IsArgumentNullException(input, i => i.Length > 0, nameof(input));
+        if (JwtErrors.IsArgumentNotEmpty(input, i => i.Length > 0, nameof(input), out error))
+        {
+            output = Convert.ToBase64String(input);
+            output = output.FirstSegment('='); // Remove any trailing '='s
+            output = output.Replace('+', '-'); // 62nd char of encoding
+            output = output.Replace('/', '_'); // 63rd char of encoding
+            return true;
+        }
 
-        var output = Convert.ToBase64String(input);
-        output = output.FirstSegment('='); // Remove any trailing '='s
-        output = output.Replace('+', '-'); // 62nd char of encoding
-        output = output.Replace('/', '_'); // 63rd char of encoding
-        return output;
+        output = null!;
+        return false;
     }
-
 
     /// <inheritdoc />
-    /// <exception cref="ArgumentException" />
+    /// <exception cref="ArgumentNullException" />
     /// <exception cref="FormatException" />
-    public byte[] Decode(string input)
+    public bool TryDecode(string input, out byte[] output, out string? error)
     {
-        Throw.IsArgumentNullException(input, StrUtil.NotEmpty, nameof(input));
-
-        var output = input;
-        output = output.Replace('-', '+'); // 62nd char of encoding
-        output = output.Replace('_', '/'); // 63rd char of encoding
-        switch (output.Length % 4) // Pad with trailing '='s
+        if (JwtErrors.IsArgumentNotEmpty(input, StrUtil.NotEmpty, nameof(input), out error))
         {
-            case 0:
-                break; // No pad chars in this case
-            case 2:
-                output += "==";
-                break; // Two pad chars
-            case 3:
-                output += "=";
-                break; // One pad char
-            default:
-                throw new FormatException("Illegal base64url string.");
+
+            input = input.Replace('-', '+'); // 62nd char of encoding
+            input = input.Replace('_', '/'); // 63rd char of encoding
+            switch (input.Length % 4) // Pad with trailing '='s
+            {
+                case 0:
+                    break; // No pad chars in this case
+                case 2:
+                    input += "==";
+                    break; // Two pad chars
+                case 3:
+                    input += "=";
+                    break; // One pad char
+                default:
+                    error = JwtErrors.ErrorTokenUrlDecode;
+                    output = null!;
+                    return false;
+            }
+            output = Convert.FromBase64String(input); // Standard base64 decoder
+            return true;
         }
-        var converted = Convert.FromBase64String(output); // Standard base64 decoder
-        return converted;
+
+        output = null!;
+        return false;
     }
+
+
+    private static string EncodeNew(byte[] input)
+    {
+        var outSize = CalcEncodeResult(input.Length);
+        var output = new char[outSize];
+        var used = Convert.ToBase64CharArray(input, 0, input.Length, output, 0);
+        //  after NetStandard2.1
+        //Span<char> output = stackalloc char[outSize];
+        //Convert.TryToBase64Chars(input, output, out var used);
+        for (var i = 0; i < used; i++)
+        {
+            var c = output[i];
+            switch (c)
+            {
+                case '+':
+                    output[i] = '-';
+                    break;
+                case '/':
+                    output[i] = '_';
+                    break;
+                case '=':
+                    used = i;
+                    break;
+            }
+        }
+
+        return new string(output, 0, used);
+    }
+
+    private static byte[] DecodeNew(string input)
+    {
+        var appendSize = (4 - input.Length % 4) % 4;
+        //if (appendSize == 1)
+        //{
+        //    Throw.Exception(new Exception(JwtErrors.ErrorTokenUrlDecode));
+        //}
+
+        var inputSpan = new char[input.Length + appendSize];
+        for (var i = 0; i < input.Length; i++)
+        {
+            var ci = input[i];
+            inputSpan[i] = ci switch
+            {
+                '-' => '+',
+                '_' => '/',
+                char c => c,
+            };
+        }
+
+        while (appendSize-- > 0)
+        {
+            inputSpan[input.Length + appendSize] = '=';
+        }
+
+        return Convert.FromBase64CharArray(inputSpan, 0, inputSpan.Length);
+    }
+
+    private static int CalcEncodeResult(int inputLength)
+        => (inputLength + 2) / 3 * 4;
 }
--- src/Simple.Web.Jwt/TokenParameters.cs
+++ src/Simple.Web.Jwt/TokenParameters.cs
@@ -5,6 +5,8 @@ namespace Simple.Web.Jwt;
 /// <summary> Contains a set of parameters that are used in the processing of token. </summary>
 public class TokenParameters
 {
+    public const string JwtSchemeName = "Bearer";
+
     #region static
 
     /// <summary> Returns a <see cref="TokenParameters" /> with all properties set to <see langword="true" />. </summary>
@@ -46,12 +48,14 @@ public class TokenParameters
     public bool ValidateIssuer { get; set; }
     public string ValidIssuer
     {
+        get => Payload.TryGet(ClaimName.Issuer, out string v) ? v : string.Empty;
         set => Payload[ClaimName.Issuer] = value;
     }
 
     public bool ValidateAudience { get; set; }
     public string ValidAudience
     {
+        get => Payload.TryGet(ClaimName.Audience, out string v) ? v : string.Empty;
         set => Payload[ClaimName.Audience] = value;
     }
 
@@ -64,7 +68,7 @@ public class TokenParameters
     public TimeSpan TimeMargin { get; set; }
 
 
-    public bool Validate(out string errorMessage)
+    public bool Validate(out string? errorMessage)
     {
         errorMessage = Algorithm is null
             ? JwtErrors.ErrorArgumentIsNull(nameof(Algorithm))
@@ -74,8 +78,101 @@ public class TokenParameters
                     ? JwtErrors.ErrorArgumentIsNull(nameof(UrlEncoder))
                     : (ValidateExpirationTime || ValidateIssuedTime) && DateTimeProvider is null
                         ? JwtErrors.ErrorArgumentIsNull(nameof(DateTimeProvider))
-                        : string.Empty;
+                        : null;
 
-        return errorMessage.Length == 0;
+        errorMessage ??= ValidateAudience && string.IsNullOrEmpty(ValidAudience) ? JwtErrors.ErrorArgumentIsInvalid(nameof(ValidAudience)) : null;
+        errorMessage ??= ValidateExpirationTime && !ValidExpiration.HasValue ? JwtErrors.ErrorArgumentIsInvalid(nameof(ValidExpiration)) : null;
+        errorMessage ??= ValidateIssuer && string.IsNullOrEmpty(ValidIssuer) ? JwtErrors.ErrorArgumentIsInvalid(nameof(ValidIssuer)) : null;
+
+        return errorMessage is null;
+    }
+}
+
+public static class ParametersExtensions
+{
+    //  Decode
+    public static bool TryCreateValidator(this TokenParameters _parameters, out IJwtValidator jwtValidator, out string? error)
+    {
+        Throw.IsArgumentNullException(_parameters, nameof(_parameters));
+
+        if (_parameters.Validate(out error))
+        {
+            jwtValidator = new JwtValidator(_parameters);
+            return true;
+        }
+
+        jwtValidator = null!;
+        return false;
+    }
+
+    public static bool TryValidateToken(this TokenParameters _parameters, string token, out IDictionary<string, object> payload, out string? error)
+    {
+        if (TryCreateValidator(_parameters, out var validator, out error) &&
+            JwtParts.TryParse(token, _parameters.UrlEncoder, out var jwt, out error) &&
+            validator.TryValidate(jwt, out error))
+        {
+            payload = jwt.EnsurePayload(_parameters.JsonSerializer);
+            return true;
+        }
+
+        payload = null!;
+        return false;
+    }
+
+    //  Encode
+    public static bool TryCreateEncoder(this TokenParameters _parameters, out IJwtEncoder encoder, out string? error)
+    {
+        _parameters = Throw.IsArgumentNullException(_parameters, nameof(_parameters));
+        if (_parameters.Validate(out error))
+        {
+            encoder = new JwtEncoder(_parameters.Algorithm!, _parameters.JsonSerializer, _parameters.UrlEncoder);
+            return true;
+        }
+
+        encoder = null!;
+        return false;
+    }
+
+    public static bool TryCreateToken(this TokenParameters _parameters, IDictionary<string, object>? claims, out string token, out string? error)
+    {
+        if (_parameters.TryCreateEncoder(out var encoder, out error))
+        {
+            var payload = MergePayload(_parameters, claims);
+            return encoder.TryEncode(payload, _parameters.SigningKey, out token, out error);
+        }
+
+        token = null!;
+        return false;
+    }
+
+
+    private static IJwtPayload MergePayload(TokenParameters _parameters, IDictionary<string, object>? claims)
+    {
+        var payload = new JwtPayload(_parameters.Payload);
+
+        if (_parameters.ValidateExpirationTime || _parameters.ValidateIssuedTime)
+        {
+            var now = _parameters.DateTimeProvider.GetNow();
+
+            if (_parameters.ValidateIssuedTime)
+            {
+                var nb = UnixEpoch.GetSecondsSince(now);
+                payload.NotBefore(nb);
+                payload.IssuedAt(nb);
+            }
+
+            if (_parameters.ValidateExpirationTime && _parameters.ValidExpiration.HasValue)
+            {
+                var exp = now + _parameters.ValidExpiration.Value;
+                payload.ExpirationTime(UnixEpoch.GetSecondsSince(exp));
+            }
+        }
+
+        if (claims is not null)
+        {
+            payload.Merge(claims);
+        }
+
+        return payload;
     }
 }
