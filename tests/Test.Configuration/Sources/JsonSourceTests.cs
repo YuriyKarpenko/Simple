28eab641b83aa38c6125684fa8cb419e46a971c9
Prog75 <mail@yk.cc.ua>
2024-10-15 13:58:43 +0300

    Unit-tests: Configuration

---
+++ tests/Test.Configuration/Sources/JsonSourceTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+using Simple.Configuration;
+using Simple.Configuration.Sources;
+
+namespace Test.Configuration.Sources;
+public class JsonSourceTests
+{
+    [Theory]
+    [InlineData("file.ext")]
+    [InlineData("/Path1/path2/file.ext")]
+    public void Build(string file)
+    {
+        const string basePath = "/base/path";
+        //  arrange
+        var props = new Dictionary<string, object>
+        {
+            { "BasePath", basePath },
+        };
+        var builder = Substitute.For<IConfigurationBuilder>();
+        builder.Properties.Returns(props);
+        var config = Substitute.For<IConfiguration>();
+        var isFile = file == Path.GetFileName(file);
+        var js = Substitute.ForPartsOf<JsonSource>();
+        //  disable exeption
+        js.IsOptional = true;
+        if (isFile)
+        {
+            js.FileName = file;
+        }
+        else
+        {
+            js.FullPath = file;
+        }
+
+        //  test
+        js.Build(builder, config);
+
+        //  assert
+        Assert.Equal(file, isFile ? js?.FileName : js?.FullPath);
+        if (isFile)
+        {
+            Assert.Equal(Path.Combine(basePath, file), js?.FullPath);
+        }
+    }
+}
