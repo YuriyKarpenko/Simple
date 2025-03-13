5cfa1df3adeafa2c1856c90779c0a17b4acd7d7c
Prog75 <mail@yk.cc.ua>
2025-03-13 15:57:13 +0200

    Helper: .Net 8.0

--- src/Simple.Helpers/ExtensionsHex.cs
+++ src/Simple.Helpers/ExtensionsHex.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace Simple.Helpers;
 
 public static class ExtensionsHex
 {
+    public const byte MaskLoByte = 0x0F;
+
+    private static readonly char[] Ten = ['A', 'a'];
+
     //  Any extensions
     public static T GetServiceRequired<T>(this IServiceProvider sp)
     {
@@ -13,29 +18,65 @@ public static class ExtensionsHex
         return Throw.IsArgumentNullException(o, typeof(T).Name);
     }
 
+
     //  HEX extensions
-    public static string ToHexString(this IEnumerable<byte> src, string byteSeparator = "")
+    [Obsolete("this overload is exclusively for reverse compatibility")]
+    public static string ToHexString(this IEnumerable<byte> src, string byteSeparator)
+        => ToHexString(src.ToArray(), false, byteSeparator);
+    public static string ToHexString(this IEnumerable<byte> src, bool isLowerCase = false, string byteSeparator = "")
     {
-        var ss = src.Select(ToHexStr).ToArray();
+        var a = src.ToArray();
+        var ss = new string[a.Length];
+        //Span<char> sb = stackalloc char[a.Length * 2];
+
+        for (var i = 0; i < a.Length; i++)
+        {
+            //var cc = a[i].ToHexChars(isLowerCase);
+            //cc.CopyTo(ss, i*2);
+
+            ss[i] = a[i].ToHexStr(isLowerCase);
+        }
+
         return string.Join(byteSeparator, ss);
     }
 
-    public static string ToHexStr(this byte b)
-    {
-        var l = ToHexChar((byte)(b & 0x0F));
-        var h = ToHexChar((byte)(b >> 4));
-        return new string([h, l]);
-    }
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string ToHexStr(this byte b, bool isLowerCase = false)
+        => new string(b.ToHexChars(isLowerCase));
 
-    public static char ToHexChar(this byte i)
+#if false && NET8_0_OR_GREATER
+    public static string ToHexChars(this byte b, bool isLowerCase = false) //where T : struct, IBitwiseOperators<byte, byte, byte>, IShiftOperators<byte, int, byte>
     {
-        if (i < 0 || i > 15)
+        var l = ToHexChar((b as IBitwiseOperators<byte, byte, byte>) & MaskLoByte, isLowerCase);
+        var h = ToHexChar(b >> 4, isLowerCase);
+        return [h, l];
+    }
+#else
+    public static char[] ToHexChars(this byte b, bool isLowerCase = false)
+    {
+        var l = ToHexChar(b.LoByte(), isLowerCase);
+        var h = ToHexChar(b.HiByte(), isLowerCase);
+        return [h, l];
+    }
+#endif
+
+    public static char ToHexChar(this byte halfByte, bool isLowerCase = false)
+    {
+        if (halfByte < 0 || halfByte > 15)
         {
-            Throw.Exception(new IndexOutOfRangeException($"ToHexChar(int): {i} must be between 0 and 15"));
+            Throw.Exception(new IndexOutOfRangeException($"ToHexChar(byte): {halfByte} must be between 0 and 15 inclusive"));
         }
 
-        return (char)(i > 9
-            ? 'A' + i - 10
-            : '0' + i);
+        return (char)(halfByte > 9
+            ? Ten[isLowerCase ? 1 : 0] + halfByte - 10
+            : '0' + halfByte);
     }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static byte HiByte(this byte b)
+        => (byte)(b >> 4);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static byte LoByte(this byte b)
+        => (byte)(b & MaskLoByte);
 }
--- src/Simple.Helpers/README.md
+++ src/Simple.Helpers/README.md
@@ -7,10 +7,10 @@ https://reubenbond.github.io/posts/dotnet-perf-tuning
 or translation
 https://www.lshnk.me/2019/01/26/улучшаем-производительность-в-.net-core-по-опыту-бинарного-сериализатора-hagar/
 
-## Using
+### Using
 
 ```cs
-using Simple;
+using Simple.Helpers;
 ...
 	Throw.Exception(new ...Exception(parameters...));
 ...
@@ -18,37 +18,87 @@ using Simple;
 	fileName = Throw.IsArgumentNullException(fileName, nameof(fileName));
 	or
 	fileName = Throw.IsArgumentNullException(fileName, nameof(fileName), i => !string.IsNullOrEmpty(i));
+
+
+	public Task<TResult> SomeMethodAsync()
+	{
+		try
+		{
+			...
+			return Task.FromResult(...); 
+		}
+		catch (Exception ex)
+		{
+			...
+			return Throw.ExceptionAsync<TResult>(ex)
+		}
+	}
+```
+
+## Option (ala Rust)
+
+### Using
+
+```cs
+using Simple.Helpers;
+class SomeClass
+{
+...
+    private async Task<IOption<IResponse>> ExecStatusAsync(ITestResult tr, IRequest request)
+    {
+        var resp = await _rest.ExecuteAsync(request);
+        return tr.Status(resp, HttpStatusCode.Accepted);
+    }
+}	
+...
+static class SomeClassExtensions
+{
+    public static IOption<IResponse> Status(this ITestResult tr, IResponse resp, HttpStatusCode expected, [CallerMemberName] string? methodName = null)
+        => tr.Eq(methodName!, resp.StatusCode, expected)
+            ? Option.Value(resp)
+            : Option.Error<IResponse>(resp.Content ?? "bad status");
+}
 ```
 
 
-### HashCoceCombiner (for netstandard2.0 only)
-class (from MS v2.2)
+## HashCoceCombiner (for netstandard2.0 only)
+class (from .NetCore v2.2)
 
-### Extensions
-some extensions for Stream
-some extensions for collectopns
-some other extensions
+## Extensions
+some extensions for Stream,
+some extensions for collectopns,
+some other extensions (string, Type)
 
 ##  Release notes
+### 8.0.0.5
+	append:
+		support net8.0
+		StrUtil.GetClassName()
+		class ExtensionsValidation
+		some methods in ExtensionsHex
+		some unit-tests
+	update:
+		refactoring CollectionExtensions class
+		refactoring Throw class
 
-0.1.1
+### 0.1.1.4
 	append:
 		support netstandard2.1
-		DicString
+		class DicString
 		Option (ala Rust)
-		RandomGenerator
-		StrUtil
+		class RandomGenerator
+		class StrUtil
 		some unit-tests
 	update:
 		CollectionExtensions (Merge IReadOnlyDictionary)
 		small fixes (in ExtensionsHex, HashCodeCombiner)
 
-0.1.0.3
+### 0.1.0.3
 	update(fixed):
 		added Throw.IsArgumentNullException override (for netstandard2.0)
 		rename Extensions => ExtensionsHex + refactoring ToHexString()
 
-0.1.0.2
+### 0.1.0.2
 	append:
 		Stream.WriteAsync()
 		Extensions (hex convert, IServiceProvider)
@@ -56,7 +106,7 @@ some other extensions
 	update:
 		added Throw.IsArgumentNullException override
 
-0.1.0.1
+### 0.1.0.1
 	append:
 		TargetFramework: net6.0
 	update:
