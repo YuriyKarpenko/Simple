1fa9eccb561284086fd4538c03ad46d0cae0b4d3
Prog75 <mail@yk.cc.ua>
2025-05-22 16:17:22 +0300

    Ttl: refactoring

--- src/Simple.Ttl/TtlValue.cs
+++ src/Simple.Ttl/TtlValue.cs
@@ -2,63 +2,61 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Simple.Ttl
+namespace Simple.Ttl;
+
+public class TtlValue<T> : TtlBase
 {
-    public class TtlValue<T> : TtlBase
+    protected T _value = default!; //  начальное значение попадает наружу только через GetOrCreate()
+
+    public TtlValue(TimeSpan ttl) : base(ttl)
     {
-        protected T? value = default;
-
-        public TtlValue(TimeSpan ttl) : base(ttl)
-        {
-        }
-
-        public T GetOrCreate(Func<T> factory)
-        {
-            lock (_lock)
-            {
-                return IsExpired
-                    ? value = EnsureValue(factory)
-                    : value!;
-            }
-        }
-
-        public T Set(T value)
-            => this.value = EnsureValue(() => value);
-
-        public bool TryGetValue(out T? value)
-        {
-            var expired = IsExpired;
-            value = expired ? default : this.value;
-            return !expired;
-        }
-
-        #region async
-
-        private Task<T?>? _taskValue = null;
-
-        public Task<T?> GetOrCreateAsync(Func<Task<T>> factory)
-        {
-            lock (_lock)
-            {
-                //  if the Value is expired and not in process getting Value
-                if (IsExpired && _taskValue == null)
-                {
-                    Interlocked.Exchange(ref _taskValue, factory().ContinueWith(SetAsync));
-                }
-            }
-            return _taskValue ?? Task.FromResult(value);
-
-            T? SetAsync(Task<T> taskValue)
-            {
-                if (taskValue.Status == TaskStatus.RanToCompletion)
-                {
-                    Set(taskValue.Result);
-                }
-                Interlocked.Exchange(ref _taskValue, null);
-                return value;
-            }
-        }
-
-        #endregion
     }
+
+    public T GetOrCreate(Func<T> factory)
+    {
+        lock (_lock)
+        {
+            return IsExpired
+                ? _value = EnsureValue(factory)
+                : _value;
+        }
+    }
+
+    public T Set(T value)
+        => _value = EnsureValue(() => value);
+
+    public bool TryGetValue(out T? value)
+    {
+        var expired = IsExpired;
+        value = expired ? default : _value;
+        return !expired;
+    }
+
+    #region async
+
+    private Task<T>? _taskValue = null;
+
+    public Task<T> GetOrCreateAsync(Func<Task<T>> factory)
+    {
+        if (_taskValue == null && IsExpired || _taskValue?.Exception != null)
+        {
+            lock (_lock)
+            {
+                Interlocked.Exchange(ref _taskValue, factory().ContinueWith(SetAsync));
+            }
+        }
+        return _taskValue ?? Task.FromResult(_value);
+    }
+
+    public T SetAsync(Task<T> taskValue)
+    {
+        if (taskValue.Status == TaskStatus.RanToCompletion)
+        {
+            Set(taskValue.Result);
+        }
+        Interlocked.Exchange(ref _taskValue, null);
+        return _value;
+    }
+
+    #endregion
 }
